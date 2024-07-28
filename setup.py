453e5a1c32115f150b46809735b663d42303dece
alex33d <69710571+alex33d@users.noreply.github.com>
2024-07-28 16:51:36 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Backtest Optimizer",  
+    version="0.1.0",
+    author="Alexnader Demachev",
+    author_email="alexdemachev@gmail.com",
+    description="Hyperparameter search with Combinatorial Cross Validation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alex33d/backtest_optimizer",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+      'pandas',
+      'sklearn',
+      'optuna',
+      'matplotlib',
+      'scipy',
+      'joblib',
+      'statsmodels'
+    ],
+)
